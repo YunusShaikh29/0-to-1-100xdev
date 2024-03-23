7b5544b90174a59b25ed00aa77b871396afbb6e7
Turbobot <turbobot@vercel.com>
2024-03-23 19:53:43 +0530

    feat(create-turbo): apply official-starter transform

---
+++ week-16-1/apps/web/app/admin/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Admin } from '@repo/ui/admin'
+
+const page = () => {
+  return (
+    <div><Admin /></div>
+  )
+}
+
+export default page
---
+++ week-16-1/packages/ui/src/admin.tsx
@@ -0,0 +1,4 @@
+"use client";
+export const Admin = () => {
+  return <div>This is the admin page</div>;
+};
